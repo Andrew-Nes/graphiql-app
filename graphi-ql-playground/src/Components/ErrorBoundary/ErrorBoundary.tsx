1d5f44d7c659ae3c05a85e7c17d42db0f798a3c6
Andrei Niasmachny <Cejosoll@gmail.com>
2023-12-08 17:32:43 +0300

    feat: add error boundary

---
+++ graphi-ql-playground/src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import React, { Component, ErrorInfo } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log({ error, errorInfo });
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
--- graphi-ql-playground/src/pages/_app.tsx
+++ graphi-ql-playground/src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import ErrorBoundary from '@/Components/ErrorBoundary/ErrorBoundary';
 import Layout from '@/Components/Layout/Layout';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ErrorBoundary>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ErrorBoundary>
   );
 }
