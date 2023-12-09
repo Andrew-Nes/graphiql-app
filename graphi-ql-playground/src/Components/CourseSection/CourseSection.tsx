ea53f3184da27578ad784257695c40b3650cb839
Here East <nastiapiven@gmail.com>
2023-12-09 01:25:43 +0100

    refactor: add keys and update styles

--- graphi-ql-playground/src/Components/CourseSection/CourseSection.tsx
+++ graphi-ql-playground/src/Components/CourseSection/CourseSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ReactElement } from 'react';
 import { LINK_COURSE, COURSE_DATA } from '../../constants';
 
@@ -9,8 +10,14 @@ function CourseSection(): ReactElement {
       <section className={styles.course}>
         <div className={styles.course__container}>
           <div className={styles.course__info}>
-            <div className={styles.course__info_picture}></div>
-            <div className={styles.course__info_text}>
+            <Image
+              className={styles.course__info_picture}
+              src="/images/course.jpg"
+              alt="Image for Course section"
+              width={260}
+              height={120}
+            />
+            <div className={styles.course__info_content}>
               <span className={styles.course__subtitle}>RS SCHOOL</span>
               <h2 className={styles.course__title}>React Course</h2>
               <div className={styles.course__body}>
@@ -30,7 +37,6 @@ function CourseSection(): ReactElement {
               </div>
             </div>
           </div>
-
           <div className={styles.course__table}>
             {COURSE_DATA.map((course, index) => (
               <div className={styles.course__table_item} key={course.title}>
