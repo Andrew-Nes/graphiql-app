6bf6e067b916da8014009ecf560878507cada494
Mikita <anykey2solo@gmail.com>
2023-12-10 13:41:35 +0300

    add login form and link to register to login page

--- graphi-ql-playground/src/pages/login.tsx
+++ graphi-ql-playground/src/pages/login.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+
+import { routes } from '@/services/routes';
+
+import LoginForm from '@/Components/Forms/LoginForm';
+
+import styles from '../styles/LoginPage.module.scss';
 
 const LoginPage: React.FC = () => {
   return (
     <>
-      <h1>Login Page</h1>
+      <LoginForm />
+      <p className={styles.registerInfo}>
+        Don’t have an account?{' '}
+        <Link href={routes.REGISTER} className={styles.loginLink}>
+          Register.
+        </Link>
+      </p>
     </>
   );
 };
