bfaadb08f3fd5627ee76365c2d19277696e125ba
Mikita <anykey2solo@gmail.com>
2023-12-12 22:45:10 +0300

    feat: redirection from pages

--- graphi-ql-playground/src/pages/playground/index.tsx
+++ graphi-ql-playground/src/pages/playground/index.tsx
@@ -1,6 +1,17 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import router from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from '@/services/auth/firebase';
+import { routes } from '@/services/routes';
 
 const PlaygroundPage: FC = () => {
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (!user) router.push(routes.MAIN);
+  }, [user]);
+
   return <h1>Playground Page</h1>;
 };
 
