9ff5d17b04cc45f5b2f610c5d7b3074cb1dfc67b
Here East <nastiapiven@gmail.com>
2023-12-22 19:03:25 +0100

    feat: create Editor component

---
+++ graphi-ql-playground/src/Components/Editor/index.tsx
@@ -0,0 +1,69 @@
+import {
+  FC,
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useRef,
+  UIEvent,
+} from 'react';
+
+import { LinesNumber } from './LinesNumber';
+
+import clsx from 'clsx';
+import styles from './Editor.module.scss';
+
+export interface EditorProps {
+  mode?: 'edit' | 'read';
+  code: string;
+  setCode?: Dispatch<SetStateAction<string>>;
+  className?: string;
+  placeholder?: string;
+}
+
+export const Editor: FC<EditorProps> = ({
+  mode = 'edit',
+  code,
+  setCode,
+  className = '',
+  placeholder = 'Type here...',
+}) => {
+  const linesNumberRef = useRef<HTMLUListElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    if (setCode) {
+      setCode(e.target.value);
+    }
+  };
+
+  const handleScroll = (e: UIEvent<HTMLUListElement | HTMLTextAreaElement>) => {
+    if (!(e.target instanceof HTMLElement)) return;
+
+    if (linesNumberRef.current && textAreaRef.current) {
+      linesNumberRef.current.scrollTop = e.target?.scrollTop;
+      textAreaRef.current.scrollTop = e.target?.scrollTop;
+    }
+  };
+
+  return (
+    <div className={clsx(styles.editor, className && styles[className])}>
+      <div className={styles.editor__container}>
+        <LinesNumber
+          code={code}
+          handleScroll={handleScroll}
+          linesNumberRef={linesNumberRef}
+        />
+
+        <textarea
+          className={styles.editor__textArea}
+          ref={textAreaRef}
+          value={code}
+          placeholder={placeholder}
+          disabled={mode === 'read'}
+          onChange={handleInput}
+          onScroll={handleScroll}
+        />
+      </div>
+    </div>
+  );
+};
