9e235f1fe45a6a0f46f9704bd44047df56472d37
Here East <nastiapiven@gmail.com>
2023-12-22 19:56:27 +0100

    refactor: add a class and styles for read mode

--- graphi-ql-playground/src/Components/Editor/LinesNumber/index.tsx
+++ graphi-ql-playground/src/Components/Editor/LinesNumber/index.tsx
@@ -1,21 +1,26 @@
 import { FC, UIEvent, RefObject } from 'react';
+import clsx from 'clsx';
 
 import styles from './LinesNumber.module.scss';
 
 export interface LinesNumberProps {
+  mode: 'edit' | 'read';
   code: string;
   linesNumberRef: RefObject<HTMLUListElement>;
   handleScroll: (e: UIEvent<HTMLUListElement | HTMLTextAreaElement>) => void;
 }
 
 export const LinesNumber: FC<LinesNumberProps> = ({
+  mode = 'edit',
   code,
   handleScroll,
   linesNumberRef,
 }) => {
   return (
     <ul
-      className={styles.linesNumber}
+      className={clsx(styles.linesNumber, {
+        [styles.readMode]: mode === 'read',
+      })}
       ref={linesNumberRef}
       onScroll={handleScroll}
     >
