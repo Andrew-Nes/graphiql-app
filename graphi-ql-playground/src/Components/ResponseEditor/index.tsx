5fcd726026d8373e5f3db15a0b5895b70189bf36
Here East <nastiapiven@gmail.com>
2023-12-22 22:59:58 +0100

    feat: create ResponseEditor component

---
+++ graphi-ql-playground/src/Components/ResponseEditor/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import { Editor } from '../Editor';
+
+import styles from './ResponseEditor.module.scss';
+
+export const ResponseEditor: FC = () => {
+  const code = `{
+  "errors": [
+    {
+      "message": "Syntax Error: Unexpected <EOF>",
+      "locations": [
+        {
+          "line": 32,
+          "column": 1
+        }
+      ]
+    }
+  ]
+}`;
+
+  return (
+    <div className={styles.responseEditor}>
+      <Editor
+        mode="read"
+        code={code}
+        placeholder="JSON response..."
+        className="editor__response"
+      />
+    </div>
+  );
+};
