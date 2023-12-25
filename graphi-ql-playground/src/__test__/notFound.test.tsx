d0c61b4692a30ac20ac7836573ca850539ce1d0b
Andrei Niasmachny <Cejosoll@gmail.com>
2023-12-25 10:50:47 +0300

    fix: fix notFound page tests

--- graphi-ql-playground/src/__test__/notFound.test.tsx
+++ graphi-ql-playground/src/__test__/notFound.test.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+import * as dictionary from '@/services/dictionary.json';
 
 import NotFound from '@/pages/404';
 import { LanguageProvider } from '@/context/languageContext';
-import { useRouter } from '@/__mocks__/router';
 
 const mockFn = jest.fn();
+
 jest.mock('next/router', () => ({
   ...jest.requireActual('next/router'),
   useRouter: jest.fn(),
 }));
 
 describe('NotFound Component', () => {
+  const useRouter = jest.spyOn(require('next/router'), 'useRouter');
   useRouter.mockReturnValue({ push: mockFn });
 
   it('renders the component correctly', () => {
@@ -23,23 +26,19 @@ describe('NotFound Component', () => {
     );
 
     expect(screen.getByText('🙃🙄️😢')).toBeInTheDocument();
-    expect(
-      screen.getByText('Oooops... Something went wrong!')
-    ).toBeInTheDocument();
-    expect(screen.getByText('Back to Home')).toBeInTheDocument();
+    expect(screen.getByText(dictionary.en.notFound.text)).toBeInTheDocument();
+    expect(screen.getByText(dictionary.en.notFound.button)).toBeInTheDocument();
   });
 
-  // it('calls router.push when the button is clicked', async () => {
-  //   render(
-  //     <LanguageProvider>
-  //       <NotFound />
-  //     </LanguageProvider>
-  //   );
-  //   const button = screen.getByText('Back to Home');
-  //   fireEvent.click(button);
+  it('calls router.push when the button is clicked', () => {
+    render(
+      <LanguageProvider>
+        <NotFound />
+      </LanguageProvider>
+    );
+    const button = screen.getByText(dictionary.en.notFound.button);
+    fireEvent.click(button);
 
-  //   await waitFor(() => {
-  //     expect(mockFn).toHaveBeenCalledWith('/');
-  //   });
-  // });
+    expect(mockFn).toHaveBeenCalledWith('/');
+  });
 });
