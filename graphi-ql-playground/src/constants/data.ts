04b1516743f42e008b044de195b4e269191de665
Mikita <anykey2solo@gmail.com>
2023-12-31 14:39:34 +0300

    feat: add default endpoint constant

--- graphi-ql-playground/src/constants/data.ts
+++ graphi-ql-playground/src/constants/data.ts
@@ -7,3 +7,5 @@ export const DEFAULT_LANG = LANGS.EN;
 
 export const LINK_SCHOOL = 'https://rs.school/';
 export const LINK_COURSE = 'https://rs.school/react/';
+
+export const DEFAULT_ENDPOINT = 'https://rickandmortyapi.com/graphql';
