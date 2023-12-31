44ef9603f6b24afe1f6d9291d4b02bbd3d503d1c
Mikita <anykey2solo@gmail.com>
2023-12-31 14:39:10 +0300

    feat: add endpoint form to playground and create endpoint state

--- graphi-ql-playground/src/pages/playground/index.tsx
+++ graphi-ql-playground/src/pages/playground/index.tsx
@@ -1,18 +1,26 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import router from 'next/router';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth } from '@/services/auth/firebase';
 import { routes } from '@/types';
+import { DEFAULT_ENDPOINT } from '@/constants';
+
+import { EndpointForm } from '@/Components/Forms/EndpointForm';
 
 const PlaygroundPage: FC = () => {
   const [user] = useAuthState(auth);
-
+  const [endpoint, setEndpoint] = useState<string>(DEFAULT_ENDPOINT);
   useEffect(() => {
     if (!user) router.push(routes.MAIN);
   }, [user]);
 
-  return <h1>Playground Page</h1>;
+  return (
+    <>
+      <h1>Playground Page</h1>
+      <EndpointForm endpoint={endpoint} endpointSetter={setEndpoint} />
+    </>
+  );
 };
 
 export default PlaygroundPage;
