f5c1edd3f68ce3d4c61d07fb9a1c7fa159f70d6f
Here East <nastiapiven@gmail.com>
2024-01-07 19:10:48 +0100

    refactor: remove line

--- graphi-ql-playground/src/__test__/requestEditor.test.tsx
+++ graphi-ql-playground/src/__test__/requestEditor.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 
-import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 
 import { LanguageProvider } from '@/context/languageContext';
