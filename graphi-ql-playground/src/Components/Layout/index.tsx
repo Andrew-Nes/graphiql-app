27ffd6bf60cb82d75ece289070884ed2b1b94b2f
Here East <nastiapiven@gmail.com>
2024-01-07 15:21:07 +0100

    feat: add loader

--- graphi-ql-playground/src/Components/Layout/index.tsx
+++ graphi-ql-playground/src/Components/Layout/index.tsx
@@ -1,17 +1,27 @@
 import { FC, ReactNode } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/services/auth/firebase';
+import clsx from 'clsx';
 
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+
+import styles from './Layout.module.scss';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
+  const [, loading] = useAuthState(auth);
+
   return (
     <div className="layout">
       <Header />
-      <main>{children}</main>
+      <main className={clsx(loading && styles.main__loading)}>
+        {loading && <span className={styles.loader}>Loading...</span>}
+        {!loading && children}
+      </main>
       <Footer />
     </div>
   );
